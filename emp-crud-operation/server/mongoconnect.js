c12cc7c1aa239816df17f4f06b4e4ed23ea1f133
NIKHIL PATIL <96325313+nikpatil2123@users.noreply.github.com>
2025-02-12 14:13:47 +0530

    Update mongoconnect.js

--- emp-crud-operation/server/mongoconnect.js
+++ emp-crud-operation/server/mongoconnect.js
@@ -8,19 +8,19 @@ dotenv.config();
 import route  from "./routes/userroute.js";
 
 const app=express();
-const port=process.env.PORT;
-const url=process.env.MONGOURL;
+const PORT=process.env.PORT;
+const URL=process.env.MONGOURL;
 app.use(cors());
 
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:false}));
 
-mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect(URL,{useNewUrlParser:true,useUnifiedTopology:true})
     .then(()=>{
         
         app.listen(port,()=>{
-        console.log(`Server is running on http://localhost:${port}`);
+        console.log(`Server is running on http://localhost:${PORT}`);
     })}
         
     )
