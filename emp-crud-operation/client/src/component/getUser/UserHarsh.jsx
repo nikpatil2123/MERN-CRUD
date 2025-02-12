4dcf009aca1f304ff51ebc2721743e2409d40dc7
NIKHIL PATIL <96325313+nikpatil2123@users.noreply.github.com>
2025-02-12 15:05:43 +0530

    Update UserHarsh.jsx

--- emp-crud-operation/client/src/component/getUser/UserHarsh.jsx
+++ emp-crud-operation/client/src/component/getUser/UserHarsh.jsx
@@ -11,7 +11,7 @@ function User() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`http://localhost:8000/api/getall`);
+        const response = await axios.get(`https://mern-crud-1huy.onrender.com/api/getall`);
         setUsers(response.data);
       }
       catch (error) {
